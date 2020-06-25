b5e2249319a1d37a736a8310718103213cc25bdf
rycnz <rycnz@outlook.com>
2020-06-25 16:53:23 +0800

    fixed page bug

--- week01/scrapy_project/scrapy_project/spiders/maoyan_movies.py
+++ week01/scrapy_project/scrapy_project/spiders/maoyan_movies.py
@@ -9,23 +9,25 @@ from scrapy_project.items import ScrapyProjectItem
 爬取电影数量在settings.py中的MOVIE_NUM修改
 '''
 
+
 class MaoyanMoviesSpider(scrapy.Spider):
     name = 'maoyan_movies'
     allowed_domains = ['maoyan.com']
     start_urls = ['https://maoyan.com/']
 
     def start_requests(self):
-        movie_num = self.settings.get('MOVIE_NUM')
-        page_num = (movie_num - 1) // 30 + 1
+        self.movie_num = self.settings.get('MOVIE_NUM')
+        page_num = (self.movie_num - 1) // 30 + 1
         for i in range(page_num):
             url = 'https://maoyan.com/films?showType=3&offset={}'.format(i * 30)
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, meta={'page': i + 1}, callback=self.parse)
 
     def parse(self, response):
-        movie_num = self.settings.get('MOVIE_NUM')
+        page = response.meta['page']
+        page_movie_num = self.movie_num - (page - 1) * 30
         selector_info = Selector(response=response)
         for i, movie_block in enumerate(selector_info.xpath('//div[@class="movie-hover-info"]')):
-            if i == movie_num:
+            if i == page_movie_num:
                 break
             movie_name = None
             movie_type = None
