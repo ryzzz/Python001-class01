81c2c54f28366e440df56f586bd5c88c228e29e4
rycnz <rycnz@outlook.com>
2020-06-30 22:41:46 +0800

    update import path

--- week02/scrapy_project/scrapy_project/spiders/maoyan_movies.py
+++ week02/scrapy_project/scrapy_project/spiders/maoyan_movies.py
@@ -2,7 +2,7 @@
 import sys
 import scrapy
 from scrapy.selector import Selector
-from scrapy_project.items import ScrapyProjectItem
+from week02.scrapy_project.scrapy_project.items import ScrapyProjectItem
 
 '''
 爬取猫眼top电影信息，存储电影名，电影类型，上映时间到scrapy_project/scrapy_result.csv中
