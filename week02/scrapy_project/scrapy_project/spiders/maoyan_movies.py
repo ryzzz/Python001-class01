0e6b2cd2b29836618d7a95a07ff8670b1ad2a813
rycnz <rycnz@outlook.com>
2020-07-03 01:33:12 +0800

    add exception handling

--- week02/scrapy_project/scrapy_project/spiders/maoyan_movies.py
+++ week02/scrapy_project/scrapy_project/spiders/maoyan_movies.py
@@ -24,28 +24,34 @@ class MaoyanMoviesSpider(scrapy.Spider):
         page_num = (self.movie_num - 1) // 30 + 1
         for i in range(page_num):
             url = 'https://maoyan.com/films?showType=3&offset={}'.format(i * 30)
-            yield scrapy.Request(url=url, meta={'page': i + 1}, callback=self.parse)
+            yield scrapy.Request(url=url, meta={'page': i + 1}, callback=self.parse, errback=self.errback)
 
     def parse(self, response):
         page = response.meta['page']
         page_movie_num = self.movie_num - (page - 1) * 30
         selector_info = Selector(response=response)
-        for i, movie_block in enumerate(selector_info.xpath('//div[@class="movie-hover-info"]')):
-            if i == page_movie_num:
-                break
-            movie_name = None
-            movie_type = None
-            movie_time = None
-            item = ScrapyProjectItem()
-            for movie_info in movie_block.xpath('./div'):
-                movie_name = movie_info.xpath('./@title').extract_first()
-                div_text = movie_info.xpath('./text()').extract()
-                span_text = movie_info.xpath('./span/text()').extract_first()
-                if span_text == '类型:':
-                    movie_type = div_text[1].strip()
-                elif span_text == '上映时间:':
-                    movie_time = div_text[1].strip()
-            item['movie_name'] = movie_name
-            item['movie_type'] = movie_type
-            item['movie_time'] = movie_time
-            yield item
+        try:
+            for i, movie_block in enumerate(selector_info.xpath('//div[@class="movie-hover-info"]')):
+                if i == page_movie_num:
+                    break
+                movie_name = None
+                movie_type = None
+                movie_time = None
+                item = ScrapyProjectItem()
+                for movie_info in movie_block.xpath('./div'):
+                    movie_name = movie_info.xpath('./@title').extract_first()
+                    div_text = movie_info.xpath('./text()').extract()
+                    span_text = movie_info.xpath('./span/text()').extract_first()
+                    if span_text == '类型:':
+                        movie_type = div_text[1].strip()
+                    elif span_text == '上映时间:':
+                        movie_time = div_text[1].strip()
+                item['movie_name'] = movie_name
+                item['movie_type'] = movie_type
+                item['movie_time'] = movie_time
+                yield item
+        except Exception as e:
+            print(e)
+
+    def errback(self, failure):
+        print(failure)
