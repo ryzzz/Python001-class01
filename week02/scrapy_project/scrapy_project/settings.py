70597b4ddf2866a3f2feebe5966a4308c3ea617c
rycnz <rycnz@outlook.com>
2020-07-03 23:49:26 +0800

    add fake user agent.

--- week02/scrapy_project/scrapy_project/settings.py
+++ week02/scrapy_project/scrapy_project/settings.py
@@ -9,6 +9,8 @@
 #     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
+from fake_useragent import UserAgent
+
 BOT_NAME = 'scrapy_project'
 
 SPIDER_MODULES = ['scrapy_project.spiders']
@@ -16,7 +18,8 @@ NEWSPIDER_MODULE = 'scrapy_project.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 # USER_AGENT = 'scrapy_project (+http://www.yourdomain.com)'
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+ua = UserAgent(verify_ssl=False)
+USER_AGENT = ua.random
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
