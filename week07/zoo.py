d088f7f371d4a6570dfde8bd3d7d797061ad16ed
rycnz <rycnz@outlook.com>
2020-08-06 23:16:00 +0800

    update single_instance function.

--- week07/zoo.py
+++ week07/zoo.py
@@ -3,7 +3,20 @@
 from abc import ABC, abstractmethod
 
 
+def single_instance(cls):
+    instances = {}
+
+    def getinstance():
+        if cls not in instances:
+            instances[cls] = cls()
+        return instances[cls]
+
+    return getinstance
+
+
 class Zoo(object):
+    animal_instance = {}
+
     def __init__(self, name):
         self.name = name
 
@@ -26,6 +39,13 @@ class Animal(ABC):
 class Cat(Animal):
     sound = '喵'
 
+    _instance = None
+
+    def __new__(cls, *args, **kargs):
+        if not cls._instance:
+            cls._instance = super(Animal, cls).__new__(cls)
+        return cls._instance
+
     def __init__(self, name, animal_type, body_type, character):
         super(Cat, self).__init__(animal_type, body_type, character)
         self.name = name
@@ -34,7 +54,19 @@ class Cat(Animal):
 
 if __name__ == '__main__':
     z = Zoo('时间动物园')
+
+    # animal = Animal('食肉', '小', '温顺')
+    # TypeError: Can't instantiate abstract class Animal with abstract methods __init__
+
     cat1 = Cat('大花猫 1', '食肉', '小', '温顺')
+    cat2 = Cat('大花猫 2', '食肉', '小', '温顺')
+    # cat1 和 cat2 为相同实例
+    print(cat1 is cat2)
+
+    # cat1 叫
+    print(cat1.sound)
+
+    # 添加动物，有猫时getattr(z, 'Cat')返回True，没有时返回False
+    print(getattr(z, 'Cat'))
     z.add_animal(cat1)
-    have_cat = getattr(z, 'Cat')
-    # print(have_cat)
+    print(getattr(z, 'Cat'))
